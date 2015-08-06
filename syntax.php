c82de1cdab481cfcf51c1ca09f7de2c419a53cfc
Matthieu Estrada <m.estrada@alpi.fr>
2015-08-06 14:09:30 +0200

    Erase error_log

--- syntax.php
+++ syntax.php
@@ -35,17 +35,14 @@ class syntax_plugin_redissue extends DokuWiki_Syntax_Plugin {
     }
 
     function handle($match, $state, $pos, $handler) {
-        error_log("redissue handle");
         switch($state){
             case DOKU_LEXER_SPECIAL :
-                error_log("redissue dans lexer_enter");
                 preg_match("/id *= *(['\"])#(\\d+)\\1/", $match, $id);
                 $id = $id[2];
                 $return = array(
                         'state'=>$state,
                         'id'=>$id
                     );
-                error_log("redissue avec id = $id");
                 return $return;
             case DOKU_LEXER_UNMATCHED :
                 return array('state'=>$state, 'text'=>$match);
@@ -55,15 +52,12 @@ class syntax_plugin_redissue extends DokuWiki_Syntax_Plugin {
     }
 
     function render($mode, $renderer, $data) {	
-        error_log("redissue render");
         if($mode != 'xhtml') return false;
 
-        error_log("redissue dans xhtml");
         $redurl = $this->geturl();
         $redurl = $redurl.$data['id'];
         switch($data['state']) {
             case DOKU_LEXER_SPECIAL :
-                error_log("redissue ecrit le a");
                 $renderer->doc .= '<a href=" ' . $redurl . '">test</a>';
             case DOKU_LEXER_UNMATCHED :
                 break;
