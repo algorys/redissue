14b59bd6455fcdf5dd89af9596818474226f10d5
Matthieu Estrada <m.estrada@alpi.fr>
2015-10-23 09:37:09 +0200

    Add choice of theme in settings

--- conf/default.php
+++ conf/default.php
@@ -3,7 +3,8 @@
  * Options for the redissue plugin
  */
 
-$conf['redissue.url'] = 'http://redmine.com';       //Set url of your redmine server
-$conf['redissue.img'] = '';
-$conf['redissue.API'] = '';
-$conf['redissue.view'] = '2';
+$conf['redissue.url']   = 'http://redmine.com';       //Set url of your redmine server
+$conf['redissue.img']   = '';
+$conf['redissue.theme'] = '6';
+$conf['redissue.API']   = '';
+$conf['redissue.view']  = '2';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2,6 +2,9 @@
 
 $lang['redissue.url']      = 'Add your redmine\'s url server, without a slash ending. Example : http://myredmine.com';
 $lang['redissue.img']      = "Type your image's url here. Example : http://www.example.com/mylogo.png";
+$lang['redissue.theme']    = 'Choose your template. The Bootstrap theme needs  <a href="https://www.dokuwiki.org/template:bootstrap3">Template Bootstrap</a> or one uses  <a href="http://getbootstrap.com/">Bootstrap</a>.';
+$lang['redissue.theme_o_4'] = "Bootstrap";
+$lang['redissue.theme_o_2'] = "Dokuwiki";
 $lang['redissue.API']      = "Enter your Redmine API's key";
 $lang['redissue.view']     = "<div>Choose the view you want to display. This will depend on the wiki user's access rights in Redmine.</div>
 <div>Impersonate : select this if your wiki's users have the same UID as Redmine's users. e.g. : LDAP authentication. Redissue then will manage rights based on private or public projects.</div>
