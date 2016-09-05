5261061b7459e145123a90c487d8998404a5378d
Matthieu Estrada <m.estrada@alpi.fr>
2016-09-05 15:03:46 +0200

    Add translation for redissue
    Ref #1 English and french

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -7,3 +7,9 @@ $lang['direction']  = 'ltr';
 
 $lang['redissue.button'] = 'Redmine Issue';
 $lang['redissue.text.default'] = 'Issue #%s';
+$lang['redissue.project'] = 'Project';
+$lang['redissue.author'] = 'Author';
+$lang['redissue.assigned'] = 'Assigned to';
+$lang['redissue.desc'] = 'Description';
+$lang['redissue.link.issue'] = 'See in Redmine';
+
