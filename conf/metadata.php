38e71254f321505957552f2d62998cbd7c332853
Matthieu Estrada <m.estrada@alpi.fr>
2017-04-27 10:29:59 +0200

    Add bootstrap themes setting
    Ref #15

--- conf/metadata.php
+++ conf/metadata.php
@@ -6,6 +6,7 @@
 
 $meta['redissue.url']   = array('string');
 $meta['redissue.img']   = array('string');
+$meta['redissue.bootstrap-themes'] = array('string');
 $meta['redissue.API']   = array('string');
 $meta['redissue.view']  = array('multichoice','_choices' => array(4,2));
 $meta['redissue.short'] = array('onoff');
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2,6 +2,7 @@
 
 $lang['redissue.url']      = 'Add your redmine\'s url server, without a slash ending. Example : http://myredmine.com';
 $lang['redissue.img']      = "Type your image's url here. Example : http://www.example.com/mylogo.png";
+$lang['redissue.bootstrap-themes'] = 'List of themes supporting "bootstrap", separated by commas. Redissue will use the "bootstrap" theme if these themes are applied. Eg. <code>bootstrap3,bootie</code>';
 $lang['redissue.API']      = "Enter your Redmine API's key";
 $lang['redissue.view']     = "<div>Choose the view you want to display. This will depend on the wiki user's access rights in Redmine.</div>
 <div>Impersonate : select this if your wiki's users have the same UID as Redmine's users. e.g. : LDAP authentication. Redissue then will manage rights based on private or public projects.</div>
