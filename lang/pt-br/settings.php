c2d5cd25f35c5f7ed6d457df3147536def291426
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:45:36 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['redissue.url']          = 'Adicione a url do seu servidor redmine, sem a barra no final. Exemplo: http://myredmine.com';
+$lang['redissue.img']          = 'Digite a url da sua imagem aqui. Exemplo: http://www.example.com/mylogo.png';
+$lang['redissue.bootstrap-themes'] = 'Lista de temas que suportam "bootstrap", separados por vírgulas. O Redissue usará o tema "bootstrap" se esses temas forem aplicados. Por exemplo. <code> bootstrap3, bootie </ code>';
+$lang['redissue.API']          = 'Insira sua chave da API Redmine';
+$lang['redissue.view']         = '<div> Escolha a visão que você deseja exibir. Isso dependerá dos direitos de acesso do usuário do wiki no Redmine. </ div>
+<div> Representar: selecione isto se os usuários do seu wiki tiverem o mesmo UID que os usuários do Redmine. por exemplo. : Autenticação LDAP. A Redissue, então, gerenciará os direitos com base em projetos privados ou públicos. </ Div>
+<div> Userview: não gerencia direitos de acesso e exibe problemas mesmo em projetos privados. </ div>';
+$lang['redissue.view_o_4']     = 'Representar';
+$lang['redissue.view_o_2']     = 'UserView';
+$lang['redissue.short']        = '(<b> Somente tema do Dokuwiki </ b>) Marque esta opção se quiser redissue no modo curto por padrão. O valor pode ser anulado pelo parâmetro [short = "0 | 1"]';
